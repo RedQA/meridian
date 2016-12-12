f2380cd702ac2b6353c29fb84f718852872b9d33
markshao <37647536@qq.com>
2016-12-12 10:18:27 +0800

    Add jquery ajax call

--- static/js/meridian.js
+++ static/js/meridian.js
@@ -1,8 +1,17 @@
 var create_project = function () {
+    var pname = $("#pname").val();
+    var gitaddr = $("#gitaddr").val();
+    var redisdb = $("#redisdb").val();
+    var fsroot = $("fsroot").val();
+    var sourcelist = $("sourcelist").val();
 
+    // aysnc call to create the project
+    $.ajax({
+        url: "/projects/",
+        async: true,
+    });
 };
 
 var clean_coverage = function () {
 
 };
-
